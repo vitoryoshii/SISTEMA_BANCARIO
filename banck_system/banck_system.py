5f682f2ad94063440a7e18fc2a068bf446637dba
vitor.yoshii <vitoryoshiiprofissional@gmail.com>
2025-04-24 23:49:44 -0300

    Refactored deposit and statement logic for better structure and readability

--- banck_system/banck_system.py
+++ banck_system/banck_system.py
@@ -15,13 +15,26 @@ def menu():
     return '''
     ========MENU========
 
-    [0] SACAR
-    [1] DEPOSITAR
+    [0] DEPOSITAR
+    [1] SACAR
     [2] EXTRATO
     [3] SAIR
 
     ====================
     '''
+
+# Função Depositar
+def depositar(saldo, extrato, valor_user, /):
+
+    # Verifica se o valor a depositar e maior que zero
+    if valor_user > 0:
+        saldo += valor_user
+        extrato += f"DEPOSITO: R$ {valor_user:.2f}\n"
+        print("\n=== Depósito realizado com sucesso! ===")
+    else:
+        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+    
+    return saldo, extrato
 
 # Função Sacar 
 def sacar(*, saldo, valor_saque, extrato, numero_saque, limite_saque, limite_saldo):
@@ -49,27 +62,8 @@ def sacar(*, saldo, valor_saque, extrato, numero_saque, limite_saque, limite_sal
 
     return saldo, extrato 
 
-# Função Depositar
-def depositar():
-
-    global saldo, extrato
-
-    print("DEPOSITO")
-    valor_deposito = float(input("Digite o valor para depositar: "))
-
-    # Verifica se o valor a depositar e maior que zero
-
-    if valor_deposito > 0:
-        saldo += valor_deposito
-        extrato += f"DEPOSITO: R$ {valor_deposito:.2f}\n"
-        print("Deposito efetuado com sucesso!")
-    else:
-        print("Opção falhou! Valor informado é inválido. ")
-
 # Função exibir extrato
-def exibir_extrato():
-
-    global extrato, saldo
+def exibir_extrato(saldo, /, *, extrato):
 
     print("\n================ EXTRATO ================")
     print("Não foram realizado movimentações." if not extrato else extrato)
@@ -82,32 +76,35 @@ while True:
 
     option = int(input(menu()))
 
-    if option == 0: # Sacar
+    if option == 0: # DEPOSITAR
+
+        print("DEPOSITO")
+        valor_user = float(input("Digite o valor para depositar: "))
+
+        saldo, extrato = depositar(saldo, extrato, valor_user)
+
+    elif option == 1: # SACAR
 
         print("SACAR")
-        valor_saque = float(input("Digite o valor para sacar: "))
+        valor_user = float(input("Digite o valor para sacar: "))
 
         saldo, extrato = sacar(
             saldo=saldo,
-            valor_saque=valor_saque,
+            valor_saque=valor_user,
             extrato=extrato,
             numero_saque=numero_saque,
             limite_saque=LIMITE_SAQUE_DIARIO,
             limite_saldo=LIMITE_SAQUE_DINEHIRO
         )
 
-    elif option == 1: # Depositar
-        
-        depositar()
-        
     elif option == 2: # Extrato 
         
-        exibir_extrato() 
+        exibir_extrato(saldo, extrato=extrato) 
 
     elif option == 3: # Sair
        
        break
 
     else:
-        print("Opção inválida! Digite novamente a opção desejada.")
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
     
